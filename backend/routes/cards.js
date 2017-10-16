12dc2e12417026915b10c40e91a9c2dbf8908327
Paul Bardea <pbardea@gmail.com>
2017-10-15 23:56:47 -0400

    Add post cards endpoint

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Sequelize = require('sequelize-cockroachdb');
 var models = require('../models');
+var uuidv4 = require('uuid/v4');
 
 router.get('/', (req, res) => {
     models.Card.findAll({
@@ -25,6 +26,13 @@ router.get('/:trip_id', (req, res) => {
 router.put('/:trip_id/:card_id', (req, res) => {
         var card = JSON.parse(req.body.card);
         models.Card.update(card, { where: {id: card.id}  });
+        res.status(200).send();
+});
+
+router.post('/', (req, res) => {
+        var card = JSON.parse(req.body.card);
+        card.id = uuidv4();
+        models.Card.create(card);
         res.status(200).send();
 });
 

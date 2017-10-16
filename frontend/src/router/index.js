3e81c867931d1d224467cc3f260d0dfd0a278eff
Yemin <yeminshou@gmail.com>
2017-10-16 12:09:13 -0400

    Add FB login and login page (#16)
    * add login auth
    
    * add title
    
    * Fix linter errors

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import CardDetailView from '@/components/cardDetailView';
-import Callback from '@/components/callback';
+import Login from '@/components/login';
 // import { requireAuth } from '../../utils/auth';
 import Trip from '@/components/trip';
 
@@ -12,11 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: CardDetailView,
-    },
-    {
-      path: '/callback',
-      component: Callback,
+      name: 'Login',
+      component: Login,
     },
     {
       path: '/trip',

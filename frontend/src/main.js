fa15429304b3b6a44a2d94934d366f68f1afb866
Paul Bardea <pbardea@gmail.com>
2017-11-14 01:27:14 -0500

    This works now. wat

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import io from 'socket.io/node_modules/socket.io-client';
+import io from 'socket.io-client';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './app';

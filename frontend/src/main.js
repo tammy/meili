23edeefe4bc6bff0609c44ec4402493827b7cab6
Paul Bardea <pbardea@gmail.com>
2017-11-14 00:49:06 -0500

    Start with connection things (#21)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import io from 'socket.io/node_modules/socket.io-client';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './app';
@@ -9,6 +10,19 @@ import { createStore } from './store';
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
+
+const socket = io('http://localhost:3333');
+socket.on('new_connection', (data) => {
+  console.log(data);
+  var time = Date();
+  const userID = 'paul' +  time;
+  const tripID = 'tripabc';
+  socket.emit('sub_trip', { user_id: userID, trip_id: tripID});
+});
+
+socket.on('update_state', (data) => {
+    console.log(data);
+});
 
 router.beforeEach((to, from, next) => {
   const authed = isLoggedIn();

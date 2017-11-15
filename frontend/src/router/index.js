248cac9c014915cca89a630be17978626132ced8
Tammy Liu <hello@tammyliu.me>
2017-11-15 17:36:46 -0500

    Trip view (#30)
    * Add a trip list view
    
    * Pass tripID via route
    
    * Added trip list navigation into trip
    
    * Fix build
    
    * Whoops, remove sublime
    
    * Ignore sublime in gitignore
    
    * Add a trip list view
    
    * Fix merge conflicts and build
    
    * Remove unused library (#32)
    
    * Add a trip list view
    
    * Fix merge conflicts again

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login';
 // import { requireAuth } from '../../utils/auth';
-import Trip from '@/views/Trip';
+import TripHome from '@/views/TripHome';
+import TripDetail from '@/views/TripDetail';
+import TripList from '@/views/TripList';
 
 Vue.use(Router);
 
@@ -15,8 +17,12 @@ export default new Router({
       component: Login,
     },
     {
-      path: '/trip',
-      component: Trip,
+      path: '/trips',
+      component: TripHome,
+      children: [
+        { path: '', name: 'TripList', component: TripList },
+        { path: ':id', name: 'TripDetail', component: TripDetail },
+      ],
     },
   ],
 });
---
+++ frontend/src/views/TripList.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="full-width">
+    <!-- Section for all trips -->
+    <div class="whitespace-top">
+      <div v-for="trip in tripsList">
+          <div class="col-xs-3 box">
+            <div class="item" v-on:click="goToTrip(trip.id)">
+            {{ trip.name }}
+            </div>
+          </div>
+      </div>
+      <div class="col-xs-3 box">
+        <div class="item hidden-hover" v-on:click="addTrip()">
+          <div class="glyphicon glyphicon-plus"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'trips-list',
+  data() {
+    return {
+      tripsList: [
+        {
+          name: 'Graduation - Paris, France',
+          id: '6347f1fc-64d1-4f8b-ac79-44d59d130b6d',
+        },
+      ],
+    };
+  },
+  methods: {
+    goToTrip(tripID) {
+      this.$router.push({
+        name: 'TripDetail',
+        params: { id: tripID },
+      });
+    },
+    addTrip() {
+      console.log('new trip');
+      // TODO: open modal?
+    },
+  },
+};
+</script>
+
+<style scoped>
+.full-width {
+  padding: 0 30px;
+  width: 100%;
+}
+
+.item {
+  margin: 10px 0;
+  padding: 20px;
+  min-height: 200px;
+  color: #31708f;
+  background-color: transparent;
+  border: 1px solid #bce8f1;
+  cursor: pointer;
+}
+
+.hidden-hover {
+  border: none;
+  color: transparent;
+}
+
+.hidden-hover:hover {
+  color: green;
+  border: 1px solid green;
+}
+
+.item:hover {
+  box-shadow: 0 0 11px rgba(33,33,33,.2); 
+}
+
+.glyphicon-plus {
+  margin: 20% 0;
+  font-size: 20px;
+}
+</style>

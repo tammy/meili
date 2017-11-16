d9d51ae56d7136d57d26609855979f4fdd084cf2
Justin Li <justinli610@hotmail.com>
2017-11-15 21:06:09 -0500

    Online user statuses: back end work to enable sending data where it needs to go (#33)

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(`/api/${VERSION}/cards/`, require('./routes/cards'));
 app.use(`/api/${VERSION}/trips/`, require('./routes/trips'));
 
 var pubSubReg = {};
-var users = [];
 
 const server = app.listen(PORT, () => {
     console.log('Starting on localhost:', PORT);    
@@ -25,28 +24,39 @@ const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
     var user = 'unpopulatedUser';
-    socket.emit('newConnection', { usersConnected: users});
-    socket.on('subTrip', (data) => {
-        console.log(data); 
+    var trip = 'unknownTrip';
+
+    socket.on('newConnection', (data) => {
+        console.log('Connection data requested');
         var userID = data['userID'];
         var tripID = data['tripID'];
         user = userID;
-        users.push(userID);
-        console.log(users);
-        io.sockets.emit('newUser', { usersConnected: users });
+        trip = tripID;
+
+        if (!pubSubReg.hasOwnProperty(tripID)) {
+            pubSubReg[tripID] = [];
+        }
+        pubSubReg[tripID].push(userID);
+
+        console.log('Currently active trips and users:');
+        console.log(pubSubReg);
+
+        socket.join(tripID);
+        io.in(tripID).emit('newUser', { usersConnected: pubSubReg[tripID] });
     });
 
-    socket.on('stateChange', (data) => {
-        console.log('STATE CHANGE!'); 
-    })
-
     socket.on('disconnect', () => {
-        console.log('Disconnect user from pubsub ' + user);
-        var index = users.indexOf(user);
+        console.log('Disconnect user from pubsub: ' + user);
+        var index = pubSubReg[trip].indexOf(user);
         if (index > -1) {
-            users.splice(index, 1);
+            pubSubReg[trip].splice(index, 1);
         }
-        socket.broadcast.emit('userDisconnected', { usersConnected: users});
+
+        if (pubSubReg[trip].length === 0) {
+            delete pubSubReg[trip];
+        }
+
+        io.in(trip).emit('userDisconnected', { usersConnected: pubSubReg[trip]});
     })
 });
 

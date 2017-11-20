964fa25c1f19158b7d11ef54e4db657bb8ecc3d4
Justin Li <justinli610@hotmail.com>
2017-11-20 13:50:31 -0500

    Convert tabs to spaces in storage.js (#47)

--- backend/utils/storage.js
+++ backend/utils/storage.js
@@ -6,34 +6,34 @@ let tripCache = require('./partialWritebackCache');
 tripCache.setupExpiryHandler(persistTrip);
 
 function persistTrip(key, value, whatChanged) {
-	console.log('[storage.js] Cache entry for trip ' + key + ' expired');
-	if (whatChanged.size > 0) {
-  	 	for (let card of value) {
-			if (whatChanged.has(card.id)) {
-				console.log('[storage.js] Writing card ' + card.id + ' to DB');
-				models.Card.update(card, { where: {id: card.id} });
-			}
-  	 	}
+    console.log('[storage.js] Cache entry for trip ' + key + ' expired');
+    if (whatChanged.size > 0) {
+        for (let card of value) {
+            if (whatChanged.has(card.id)) {
+                console.log('[storage.js] Writing card ' + card.id + ' to DB');
+                models.Card.update(card, { where: {id: card.id} });
+            }
+        }
 
-  	 	// Put the trip back into the cache
-  	 	tripCache.setEntry(key, value);
-  	 	return;
-  	}
+        // Put the trip back into the cache
+        tripCache.setEntry(key, value);
+        return;
+    }
 }
 
 // Fetch trip cards
 module.exports.withCardsFromTrip = (tripId, callback) => {
-	let trip = tripCache.retrieveEntry(tripId);
-	if (trip !== undefined) {
-		console.log('[storage.js] Fetching trip: trip was cached');
-		callback(trip);
-		return;
-	}
+    let trip = tripCache.retrieveEntry(tripId);
+    if (trip !== undefined) {
+        console.log('[storage.js] Fetching trip: trip was cached');
+        callback(trip);
+        return;
+    }
 
-	console.log('[storage.js] Fetching trip from DB and caching it');
+    console.log('[storage.js] Fetching trip from DB and caching it');
 
-	// Pull data from database
-	models.Card.findAll({
+    // Pull data from database
+    models.Card.findAll({
         where: {
             trip: tripId,
         }, 
@@ -47,33 +47,33 @@ module.exports.withCardsFromTrip = (tripId, callback) => {
 
 // TODO: hook into cache
 module.exports.withTripDetails = (tripId, callback) => {
-	models.Trip.findById(tripId).then((trip) => {
+    models.Trip.findById(tripId).then((trip) => {
         callback(trip.dataValues);
     });
 }
 
 module.exports.updateCard = (tripId, newCard, callback) => {
-	module.exports.withCardsFromTrip(tripId, (trip) => {
-		// Update cached value
-		for (let i = 0; i < trip.length; i++) {
-			console.log('[storage.js] ' + trip[i].id);
-			if (trip[i].id = newCard.id) {
-				console.log('[storage.js] Mutating cached trip list with update for card ' + trip[i].id);
-				trip[i] = newCard;
-				// TODO: maybe consider passing in a Set instead of a list since the callback passes back a Set
-  	 			tripCache.markAsUpdated(tripId, [trip[i].id]);
-				break;
-			}
-  	 	}
-  	 	callback(newCard);
-	});	
+    module.exports.withCardsFromTrip(tripId, (trip) => {
+        // Update cached value
+        for (let i = 0; i < trip.length; i++) {
+            console.log('[storage.js] ' + trip[i].id);
+            if (trip[i].id = newCard.id) {
+                console.log('[storage.js] Mutating cached trip list with update for card ' + trip[i].id);
+                trip[i] = newCard;
+                // TODO: maybe consider passing in a Set instead of a list since the callback passes back a Set
+                tripCache.markAsUpdated(tripId, [trip[i].id]);
+                break;
+            }
+        }
+        callback(newCard);
+    }); 
 }
 
 module.exports.addCard = (tripId, newCard, callback) => {
-	// TODO: implement caching
-	tripCache.invalidateEntry(tripId);
+    // TODO: implement caching
+    tripCache.invalidateEntry(tripId);
 
-	models.Card.create(newCard).then(() => {
+    models.Card.create(newCard).then(() => {
         models.Card.findAll({
             where: {
                 id: newCard.id,
@@ -86,7 +86,7 @@ module.exports.addCard = (tripId, newCard, callback) => {
 }
 
 module.exports.getAllCards = (callback) => {
-	models.Card.findAll({
+    models.Card.findAll({
         order: ['order'],
         raw: true,
     }).then((cards) => {

b36a8db4e28ebc1be959cfa5d776b26d9f2236b9
Tammy Liu <tammy.liu.x@gmail.com>
2017-11-21 14:31:54 -0500

    Adding collaborators, adding trips, UI changes (#55)
    * Polish trips list a bit
    
    * Added collaborators dialogue
    
    * Connected to backend API, does not quite work yet
    
    * Added shadows to the trip timeline and details view
    
    * Add create trip and try to hook it up with the backend, WIIP

---
+++ frontend/src/components/Collaborators.vue
@@ -0,0 +1,158 @@
+<!-- HTML -->
+<template>
+  <div class="online-box">
+    <div class="user-row">
+      <div v-for="user in users"><span></span>
+            <!-- TODO: popover name of the person -->
+          <!-- <span id="popoverData" data-toggle="popover" :data-content="user.displayName" data-placement="right" data-trigger="hover"> -->
+        <div class="avatar">
+          <img :src="user.profileImageUrl" :alt="user.displayName"></img>
+        </div>
+      </div>
+      <div class="avatar add hidden-hover" v-on:click="showCollaboratorModal()">
+        <div class="glyphicon glyphicon-plus"></div>
+      </div>
+    </div>
+
+    <!-- Add Collaborator Modal -->
+    <modal name="add-collaborator" height="auto">
+      <div style="padding: 20px 30px">
+        <h4>Collaborators</h4>
+        <hr/>
+        <div v-for="colab in collaborators">
+          <div class="collaborators">
+            {{ colab.name }} ( {{ colab.email }} )
+          </div>
+        </div>
+
+        <input type="email" v-model="email" placeholder="name@domain.com"></input>
+        <button class="btn btn-success" v-on:click="addCollaborator()">Add</button>
+        <div class="alert alert-success" v-if="success">
+          <strong>Success!</strong> Collaborator {{email}} has been added.
+        </div>
+      </div>
+    </modal>
+  </div>
+</template>
+
+
+<!-- JavaScript -->
+<script>
+export default {
+  name: 'collaborators',
+  data() {
+    return {
+      email: null,
+      success: true,
+    };
+  },
+  computed: {
+    users() {
+      const listOfUserData = Object.keys(this.$store.state.onlineUsers).map((key) => {
+        return this.$store.state.onlineUsers[key];
+      });
+      return listOfUserData;
+    },
+    collaborators() {
+      this.$store.dispatch('getCollaborators');
+      return this.$store.state.trip.collaborators;
+    },
+  },
+  methods: {
+    showCollaboratorModal() {
+      this.$modal.show('add-collaborator');
+    },
+    addCollaborator() {
+      this.$store.dispatch('addCollaborator', this.email);
+      // TODO: watch for the success of this call
+      // this.success = true;
+    },
+  },
+};
+</script>
+
+<!-- CSS -->
+<style scoped>
+.alert {
+  margin-top: 10px;
+}
+
+hr {
+  border-color: #bce8f1;
+}
+
+input {
+  width: calc(100% - 90px);
+  margin: 10px 10px 0 10px;
+  border: none;
+  border-bottom: 3px dashed #bce8f1;
+  height: 34px;
+}
+
+input:focus {
+  outline: none;
+}
+
+.collaborators {
+  margin-left: 10px;
+  font-size: 12pt;
+  margin-bottom: 5px;
+}
+
+.online-box {
+  padding: 10px 0;
+  text-align: left;
+}
+
+.avatar {
+  border: 2px solid #bce8f1;
+  border-radius: 50%;
+  display: inline-block;
+}
+
+img {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+}
+
+/* Space only between elements */
+.avatar {
+  margin-top: 10px;
+}
+
+.add {
+  border: 1px solid green;
+  width: 40px;
+  height: 40px;
+  color: green;
+  cursor: pointer;
+  padding: 10px 0 0 12px;
+}
+
+.hidden-hover {
+  border: none;
+  color: transparent;
+}
+
+.hidden-hover:hover {
+  color: green;
+  border: 1px solid green;
+}
+
+/* Tooltip text */
+.tooltip2 .tooltiptext {
+    visibility: hidden;
+    background-color: #31708f;
+    color: #fff;
+    text-align: center;
+    height: 50px;
+    width: 50px;
+    position: absolute;
+    z-index: 1;
+}
+
+.tooltip2:hover .tooltiptext {
+    visibility: visible;
+}
+</style>

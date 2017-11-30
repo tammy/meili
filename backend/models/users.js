2cec808c72fa86daea46cefed3bde273a3471009
Paul Bardea <pbardea@gmail.com>
2017-11-29 21:00:29 -0500

    Read only users backend and frontend (#96)
    * Backend apis added
    
    * display trip properly based on your backend permissions
    
    * Shorten if statements
    
    * Update endpoint
    
    * Fix backend hookup
    
    * Fix read only parsing
    
    * properly handle undefined

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,10 +3,12 @@ var models = require('../models');
 var uuidv4 = require('uuid/v4');
 var storage = require('../utils/storage');
 
-module.exports.addUserToTripByUserId = (tripId, userId, callback) => {
+module.exports.addUserToTripByUserId = (tripId, userId, readOnly, callback) => {
+  var readOnlyPermission = readOnly === true;
   const newRelation = {
-    userId,
-    tripId,
+    userId: userId,
+    tripId: tripId,
+    readOnly: readOnlyPermission,
   };
   models.UserTrip.create(newRelation).then(callback);
 };
